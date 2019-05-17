70a13f0d8efb74e337cfbeb60bf6642fa6497a4a
ToluFash <faithbullock4@gmail.com>
2019-05-17 23:33:06 +0100

    initial commit

---
+++ test/test.js
@@ -0,0 +1,5 @@
+document.getElementById('click1').addEventListener('click',() =>{modalize('Hello World','#e2da7d','#6b77d6', 'small Arial')});
+document.getElementById('click2').addEventListener('click',() =>{modalize('Hello WorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorldWorld','#e2da7d','#6b77d6', 'small Arial')});
+document.getElementById('click3').addEventListener('click',() =>{modalize('Hello World','#e2794b','#76d64e')});
+document.getElementById('click4').addEventListener('click',() =>{modalize('Hello World','#e2da7d')});
+document.getElementById('click5').addEventListener('click',() =>{modalize('Hello World')});
